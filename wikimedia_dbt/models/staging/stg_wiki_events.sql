725ffb7e8f8b45168d9afda11250238bc919a7ca
Khalil Chikhi <khalilchikhi018@gmail.com>
2026-05-18 11:34:41 +0100

    dbt connected to databricks, marts running on cloud

--- wikimedia_dbt/models/staging/stg_wiki_events.sql
+++ wikimedia_dbt/models/staging/stg_wiki_events.sql
@@ -1,37 +1,33 @@
 with source as (
 
-    select * from delta_scan('C:/tmp/delta/wikimedia_events')
+    select * from workspace.default.wikimedia_events
 
 ),
 
 cleaned as (
 
     select
-        cast(id as varchar)         as event_id,
-        type                        as event_type,
-        title                       as page_title,
+        event_id,
+        event_type,
+        page_title,
         namespace,
-        user                        as editor,
+        editor,
         is_bot,
+        editor_type,
         wiki,
         server_name,
         comment,
         event_time,
         processed_at,
 
-        case
-            when is_bot = true then 'bot'
-            else 'human'
-        end                         as editor_type,
-
-
-        date_trunc('hour', event_time) as event_hour,
-        date_trunc('day', event_time)  as event_date
+        date_trunc('hour', event_time)  as event_hour,
+        date_trunc('day', event_time)   as event_date
 
     from source
     where event_time is not null
-      and type in ('edit', 'new', 'categorize', 'log')
-      and id is not null
+      and event_type in ('edit', 'new', 'categorize', 'log')
+      and event_id is not null
+
 )
 
 select * from cleaned
